51ec848d58ec9e1ee868d0699b37717ecea9649a
amuluze <wangjialong89@yeah.net>
2022-09-26 18:56:34 +0800

    feat: update redis

---
+++ redis/default.go
@@ -0,0 +1,5 @@
+// Package redis
+// Date: 2022/9/26 17:51
+// Author: Amu
+// Description:
+package redis
---
+++ redis/default_test.go
@@ -0,0 +1,5 @@
+// Package redis
+// Date: 2022/9/26 17:56
+// Author: Amu
+// Description:
+package redis
