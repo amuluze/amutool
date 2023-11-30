720d7ee69578cfee8301977c164fecc8c9d7f29b
jialong.wang <jialong.wang@chaitin.com>
2023-11-30 17:37:47 +0800

    fix: default consumer offset initial

--- kafka/scripts/docker-kafka-consumers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#################################
-# Author     : Amu
-# Date       : ${DATE} ${TIME}
-# Description: 重启服务
-#################################
-
-docker exec -i kafka /opt/bitnami/kafka/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
--- kafka/scripts/docker-kafka-delete-topic.sh
+++ kafka/scripts/docker-kafka-delete-topic.sh
@@ -2,7 +2,7 @@
 #################################
 # Author     : Amu
 # Date       : ${DATE} ${TIME}
-# Description: 重启服务
+# Description: 删除一个 topic
 #################################
 
 docker exec -i kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic $1
