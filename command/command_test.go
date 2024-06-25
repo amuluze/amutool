92c6a150116b6c85a62213578294c26b29fb2d94
jialong.wang <jialong.wang@chaitin.com>
2024-06-25 09:56:08 +0800

    feat: update command

--- command/command_test.go
+++ command/command_test.go
@@ -20,7 +20,7 @@ func TestRunCommand(t *testing.T) {
 }
 
 func TestRunCommandWithBlock(t *testing.T) {
-	if res, err := RunCommandWithBlock(context.TODO(), "ls"); err != nil {
+	if res, err := RunCommandWithTimeout(context.TODO(), 2, "ls"); err != nil {
 		fmt.Printf("error: %v\n", err)
 	} else {
 		fmt.Printf("res: \n%v\n", res)
